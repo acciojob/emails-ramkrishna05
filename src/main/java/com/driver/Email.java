fc84093d43dbe8953365bb82e0171370e7226c48
ramkrishna_05 <ramkrishnapatel306@gmail.com>
2023-11-27 17:28:12 +0530

    second

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -25,7 +25,7 @@ public class Email {
         // 3. It contains at least one lowercase letter
         // 4. It contains at least one digit
         // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
-        if(password==newPassword)
+        if(password.equals(oldPassword))
         {
             if(newPassword.length()>=8 && check(newPassword))
             {
